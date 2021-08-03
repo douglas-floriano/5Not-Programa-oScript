0a3b9e5c5f96eb2b2f009e616d84e0c7bc17ab44
douglas-floriano <doudouglasglascosta@gmail.com>
2021-08-03 01:30:56 +0000

    Iniciando Novo Projeto React

---
+++ menu_horizontal/src/App.js
@@ -0,0 +1,29 @@
+import React from "react"
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <section id='inicio'>
+        <h2>Início</h2>
+
+      </section>
+      <section id="cursos">
+        <h3> Cursos </h3>
+
+      </section>
+    </>
+  );
+}
+
+export default App;
+
+
+/* seções:
+1 - início - header  - menu
+2- cursor
+3- contatos
+4- parceiros
+5- missão
+6- rodapé - footer
+*/
