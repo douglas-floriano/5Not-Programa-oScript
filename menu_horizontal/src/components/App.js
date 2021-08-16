0c87c2b202ddca4b71a762676d681c807142debe
douglas-floriano <doudouglasglascosta@gmail.com>
2021-08-16 23:51:01 +0000

    Atualização

--- menu_horizontal/src/components/App.js
+++ menu_horizontal/src/components/App.js
@@ -1,13 +1,25 @@
-import React from 'react';
+import React from 'react'
 import './App.css';
-import Secoes from './Secoes.js'
+import Sections from './Sections.js'
 
 function App() {
   return (
-    <div >
-      <Secoes/>
+    <div>
+
+      <Sections />
+
     </div>
   );
 }
 
 export default App;
+
+
+/* Seções:
+1 - inicio/header
+2 - cursos
+3 - contatos
+4 - parceiros
+5 - missão
+6 - rodapé/footer
+*/
--- menu_horizontal/src/index.js
+++ menu_horizontal/src/index.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from '../src/components/App';
+import App from './Components/App';
 
 ReactDOM.render(
   <div>
+    
     <App />
+    
   </div>,
   document.getElementById('root')
 );
-
-
-
